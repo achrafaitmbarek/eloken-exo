b3ec4bfb14dfc1d7477f554240274d05a142abaa
achraf ait m'barek <achrafaitmbarek@achrafs-MacBook-Air.local>
2024-01-02 23:14:49 +0100

    testing the git  repo and initializing the data file

---
+++ src/app/data/data.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+      title: "Pompe à chaleur 1",
+      powerRange:
+        {
+          min: 1000,
+          max: 4999
+        },
+      price: 3456,
+      image: "",
+      pacType: 'Air/Eau',
+      gasType: "R330",
+      warranty: 2,
+      isPacSystemModule: true,
+    },
+    {
+      title : "Pompe à chaleur 2",
+      powerRange:
+        {
+          min: 5000,
+          max: 10000
+        },
+      price: 5678,
+      image: "",
+      pacType: 'Air/Air',
+      gasType: "R430",
+      warranty: 3,
+      isPacSystemModule: true,
+    },
+    {
+      title : "Pompe à chaleur 3",
+      powerRange:
+        {
+          min: 10000,
+          max: 15000
+        },
+      price: 7890,
+      image: "",
+      pacType: 'Air/Eau',
+      gasType: "R380",
+      warranty: 4,
+      isPacSystemModule: true,
+    },
+    {
+      title : "Pompe à chaleur 4",
+      powerRange:
+        {
+          min: 15000,
+          max: 30000
+        },
+      price: 1234,
+      image: "",
+      pacType: 'Air/Air',
+      gasType: "R380",
+      warranty: 5,
+      isPacSystemModule: true,
+    }
+
+  ]
