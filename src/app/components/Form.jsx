d0a0f07629bc262be0213bf7e1104021a8c19f1a
achraf ait m'barek <achrafaitmbarek@achrafs-MacBook-Air.local>
2024-01-02 23:40:16 +0100

    repo problem fixed

---
+++ src/app/components/Form.jsx
@@ -0,0 +1,14 @@
+'use client';
+import React from "react";
+
+
+const Form = () => {
+  return (
+    <>
+      <div>Hello From The Form</div>
+      <button onClick={()=>{console.log('helllo mtf i am working')}}>click me </button>
+    </>
+  );
+};
+
+export default Form;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Form from './components/Form'
 
 export default function Home() {
   return (
-    <h1> To make sure everything is okey Hello from the Eloken Exo alternance !!!</h1>
-  )
+    <Form/>
+    )
 }
