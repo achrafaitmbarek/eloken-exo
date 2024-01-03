c90aaaa04163983752c68ff065a1cead9d562b7f
achraf ait m'barek <achrafaitmbarek@achrafs-MacBook-Air.local>
2024-01-03 01:30:51 +0100

    start figuring out the project components and adding tailwind to the project

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+'use client'
 import Image from 'next/image'
-import Form from './components/Form'
+import Form from './components/stepForm'
 
 export default function Home() {
   return (
-    <Form/>
+  <div className='container mx-auto'> 
+    <h1 className="text-3xl font bold ">Composez votre kit pompe a chaleur</h1>
+  </div>
     )
 }
