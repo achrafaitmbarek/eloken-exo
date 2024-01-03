4271742049f80e12c2d691bd3d16dd3e64ba867a
achraf ait m'barek <achrafaitmbarek@achrafs-MacBook-Air.local>
2024-01-03 03:37:00 +0100

    nothing special just a quick storage manipulation hh ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 'use client'
 import Image from 'next/image'
-import Form from './components/stepForm'
 import products from './data/data'
+import React,{ useEffect } from 'react'
 
 export default function Home() {
+  useEffect(()=>{
+    localStorage.setItem('products', JSON.stringify(products));
+    console.log(storedProducts);
+  },[]);
+  const storedProducts = JSON.parse(localStorage.getItem('products'));
   return (
   <div className='container mx-auto'> 
     <h1 className="text-3xl font bold ">Composez votre kit pompe a chaleur</h1>
@@ -14,6 +19,9 @@ export default function Home() {
           <p className="font-bold py-5">{product.powerRange.max} / {product.powerRange.min}</p>
         </div>
    ))} 
+   <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md'
+            onClick={()=>{localStorage.removeItem('products');
+                           console.log('storage cleared')}}>clear local Storage</button>
     </div>
   
   </div>
