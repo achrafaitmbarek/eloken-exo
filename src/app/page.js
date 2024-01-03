38062a51761ded8117681bd7cd9c1dc1eb52adc7
achraf ait m'barek <achrafaitmbarek@achrafs-MacBook-Air.local>
2024-01-03 03:40:05 +0100

    nothing special just a quick storage manipulation hh ... 2 good night see you tomorrow ... Zzz

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,16 @@ import products from './data/data'
 import React,{ useEffect } from 'react'
 
 export default function Home() {
-  useEffect(()=>{
-    localStorage.setItem('products', JSON.stringify(products));
-    console.log(storedProducts);
-  },[]);
+  // useEffect(()=>{
+  //   localStorage.setItem('products', JSON.stringify(products));
+  //   console.log(storedProducts);
+  // },[]);
   const storedProducts = JSON.parse(localStorage.getItem('products'));
   return (
   <div className='container mx-auto'> 
     <h1 className="text-3xl font bold ">Composez votre kit pompe a chaleur</h1>
     <div className="grid gap-3 grid-cols-2">
-       {products.map((product, index)=>(
+       {storedProducts.map((product, index)=>(
         <div>
           <h2 key={index} className="text-2xl font-bold py-5">{product.title}</h2>
           <p className="font-bold py-5">{product.powerRange.max} / {product.powerRange.min}</p>
