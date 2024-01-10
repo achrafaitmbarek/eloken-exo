00d5774df465ae0b96de869160a57dcb368e0f9c
achraf ait m'barek <achrafaitmbarek@achrafs-MacBook-Air.local>
2024-01-10 04:43:01 +0100

    adding some animations

--- src/app/components/modal.jsx
+++ src/app/components/modal.jsx
@@ -31,7 +31,7 @@ const Modal = ({ show, onCancel, onConfirm }) => {
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto animate-fade-in-scale">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
